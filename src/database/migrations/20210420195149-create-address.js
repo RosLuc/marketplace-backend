e37f8ee952078b39b310ef06d5c64d718af6f6d8
Anderson Pinheiro <andersonfernandespinheiro@gmail.com>
2021-04-20 18:10:28 -0300

    estrutura account e address

--- src/database/migrations/20210420195149-create-address.js
+++ src/database/migrations/20210420195149-create-address.js
@@ -3,7 +3,39 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('address', { 
-      id: Sequelize.INTEGER 
+      id: {
+        value: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      uf: {
+        value: Sequelize.STRING,
+        allowNull: false
+      },
+      cep: {
+        value: Sequelize.STRING,
+        allowNull: false
+      },
+      address: {
+        value: Sequelize.STRING,
+        allowNull: false
+      },
+      comp: {
+        value: Sequelize.STRING,
+        allowNull: false
+      },
+      district: {
+        value: Sequelize.STRING,
+        allowNull: false
+      },
+      number: {
+        value: Sequelize.INTEGER,
+        allowNull: false
+      },
+      city: {
+        value: Sequelize.STRING,
+        allowNull: false
+      }
     });
   },
 
--- src/database/migrations/20210420195218-create-account.js
+++ src/database/migrations/20210420195218-create-account.js
@@ -3,7 +3,29 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('account', { 
-      id: Sequelize.INTEGER 
+      id: {
+        value: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      number_card: {
+        value: Sequelize.STRING,
+        allowNull: false
+      },
+      validate_date: {
+        value: Sequelize.DATE,
+        set: () => {
+          if(this.validate_date === null) this.validate_date = new Date();
+        }
+      },
+      verify_nuber: {
+        value: Sequelize.INTEGER,
+        allowNull: false
+      },
+      name: {
+        value: Sequelize.STRING,
+        allowNull: false
+      }
     });
   },
 
